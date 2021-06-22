46ae43014791564898004276aab05ac6b629ef8e
R. Sivera <r.sivera@ucl.ac.uk>
2021-06-22 07:18:46 +0100

    few updates on CLI: no-check, show-pw, do-pca options

--- python/main_ssm.py
+++ python/main_ssm.py
@@ -15,10 +15,13 @@ Options:
     -n NAME, --name NAME        Object name [default: geom]
     -t MESH, --template MESH    Initial guess, int or path [default: 0]
 
-    --kwg FLOAT        kernel width of the geometry [default: 10.]
-    --kwd FLOAT        kernel width of the diffeomorphisms [default: 20.]
-    --noise FLOAT      noise [default: 10.]
-    --show
+    --kwg FLOAT         kernel width of the geometry [default: 10.]
+    --kwd FLOAT         kernel width of the diffeomorphisms [default: 20.]
+    --noise FLOAT       noise [default: 10.]
+
+    --show-pw           try to open paraview to show the results
+    --no-check          skip initialisation check
+    --do-pca            compute PCA modes and shoot on 0 and 1.
 """
 
 
@@ -47,7 +50,6 @@ def input_parameters():
         params["--odir"] = config.get("paths", "odir").strip('"')
         params["--name"] = config.get("paths", "name").strip('"')
         params["--template"] = config.get("paths", "initial_guess").strip('"')
-
         params["--kwg"]   = config.get("parameters", "kernel_width_geometry")
         params["--kwd"]   = config.get("parameters", "kernel_width_deformation")
         params["--noise"] = config.get("parameters", "noise_geometry")
@@ -71,38 +73,42 @@ if __name__ == "__main__":
         noise=params["--noise"])
 
     # Atlas estimation
-    ae.check_initialisation()
+    if not params["--no-check"]:
+        ae.check_initialisation()
     ae.estimate()
+    ae.save_parameters()
 
-    if params["--show"]:
+    # Results
+    if params["--show-pw"]:
         try:
             sp.call(["paraview", "--data=" + ae.odir + "/output/DeterministicAtlas__EstimatedParameters__Template_" + ae.id + ".vtk"])
         except FileNotFoundError:
             pass
 
     # PCA
-    ao = ssm_pca.DeformetricaAtlasPCA(
-        idir = ae.odir + "output/",
-        odir = ae.odir + "pca/")
-    ao.compute_pca(with_plots=True)
-    f0 = ao.save_eigv(0, with_controlpoints=True)
-    f1 = ao.save_eigv(1, with_controlpoints=True)
+    if params["--do-pca"]:
+        ao = ssm_pca.DeformetricaAtlasPCA(
+            idir = ae.odir + "output/",
+            odir = ae.odir + "pca/")
+        ao.compute_pca(with_plots=True)
+
+        f0 = ao.save_eigv(0, with_controlpoints=True)
+        f1 = ao.save_eigv(1, with_controlpoints=True)
+
+        ae.shooting(f0 + ".txt", ae.odir + "pca/shoot0/")
+        ae.shooting(f1 + ".txt", ae.odir + "pca/shoot1/")
+        
+        #ae.render_momenta_norm(ao.get_eigv(0))
+        if params["--show-pw"]:
+            try:
+                sp.call(["paraview", "--data=" + ae.odir + "pca/shoot0/Shooting__GeodesicFlow__" + ae.id + "_tp..vtk"])
+            except FileNotFoundError:
+                pass
+
+
 
     # Registering a new subject
-    fmesh = ae.get_path_data(0)
-    ae.registration(fmesh, ae.odir + "registration/", subject_id="subj")
-
-    fmomenta = ae.odir + "registration/DeterministicAtlas__EstimatedParameters__Momenta.txt"
-    fig = ao.project_subject_on_pca(fmomenta)
-
-    # Visualisation of the PCA modes on template
-    ae.shooting(f0 + ".txt", ae.odir + "pca/shoot0/")
-    ae.shooting(f1 + ".txt", ae.odir + "pca/shoot1/")
-
-    if params["--show"]:
-        try:
-            sp.call(["paraview", "--data=" + ae.odir + "pca/shoot0/Shooting__GeodesicFlow__" + ae.id + "_tp..vtk"])
-        except FileNotFoundError:
-            pass
-
-        ae.render_momenta_norm(ao.get_eigv(0))
+    #fmesh = ae.get_path_data(0)
+    #ae.registration(fmesh, ae.odir + "registration/", subject_id="subj")
+    #fmomenta = ae.odir + "registration/DeterministicAtlas__EstimatedParameters__Momenta.txt"
+    #fig = ao.project_subject_on_pca(fmomenta)
